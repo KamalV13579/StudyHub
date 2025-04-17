ad7face6d8c41330431f5b1c9f2c3d1d47f8f154
nickolasbleykhman <nbleykhman@icloud.com>
2025-04-17 16:12:55 -0400

    added dummy code to resource, forum, and tutoring session pages

---
+++ web/pages/course/[courseId]/tutoring/[sessionId].tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+// THIS PAGE IS NOT YET IMPLEMENTED
+// THIS IS DUMMY CODE FOR NOW
+
+export default function TutoringSessionPage() {
+  return (
+    <div>
+      <h1>Tutoring</h1>
+      <p>Course ID: course</p>
+      <p>Tutoring ID: tutoring</p>
+      {/* TODO: implement real Tutoring UI */}
+    </div>
+  );
+}
